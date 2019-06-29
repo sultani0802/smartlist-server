9125359ae9a709251cb5b46812eca9ac96c1bf57
Haamed Sultani <haamedsultani@Haameds-2018-MacBook-Pro.local>
2019-06-29 15:03:19 -0400

    moved routers to the right spot in index.js

--- src/index.js
+++ src/index.js
@@ -16,10 +16,13 @@ const app = express()                   // Init express
 const PORT = process.env.PORT || 3000   // Set port for server
 app.use(express.json())                 // Automatically parse incoming JSON into js                                            objects
 
+/**
+ * Endpoints from routers
+ */
+app.use(userRouter)
+
 
 // Run the server
 app.listen(PORT, () => {
     log('Server is running on port ', PORT)
 })  
-
-app.use(userRouter)
