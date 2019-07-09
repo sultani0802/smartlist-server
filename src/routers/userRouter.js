722cf214b51f5c4a06ba48021a43b2fbb69c289b
Haamed Sultani <haamedsultani@Haameds-2018-MacBook-Pro.local>
2019-07-09 18:59:41 -0400

    updated error message for login route

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -53,7 +53,7 @@ router.post('/users/login', async (req, res) => {
 
         res.send({user, newToken})              // Respond with the matching User Document
     } catch (e) {
-        res.status(400).send(e)     // Error
+        res.status(400).send(new Error('Invalid credentials'))     // Error
     }
 })
 
